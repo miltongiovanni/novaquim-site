780f106ef1ba783da77e6d8b49aa52d8b7dc0163
miltongiovanni <miltongiovanni@gmail.com>
2022-04-17 07:37:48 -0400

    Fix formato footer

--- inc/footer.php
+++ inc/footer.php
@@ -8,35 +8,35 @@ $configuration = $stmt->fetchAll(PDO::FETCH_KEY_PAIR);
 
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-3 navbar-dark">
                 <h5>Contáctenos</h5>
-                <ul class="list-unstyled">
-                    <li class="mt-2">
-                        <i class="fas fa-phone"></i>&nbsp;&nbsp;(57 1) 203 9484 - (57 1) 202 2912
+                <ul class="flex-column navbar-nav">
+                    <li class="nav-item">
+                        <i class="fas fa-phone"></i>&nbsp;&nbsp;(57 601) 203 9484 - (57 601) 202 2912
                     </li>
-                    <li class="mt-2">
+                    <li class="nav-item">
                         <i class="fas fa-at"></i>&nbsp;&nbsp;
-                        <span id="emq"><a href="mailto:info@novaquim.com">info@novaquim.com</a></span>
+                        <span id="emq"><a class="nav-link d-inline-block" href="mailto:info@novaquim.com">info@novaquim.com</a></span>
                     </li>
-                    <li class="mt-2">
+                    <li class="nav-item">
                         <i class="fas fa-home"></i> &nbsp; Calle 35 C sur No. 26F - 40<br> Zona Industrial Bravo Páez
                     </li>
                 </ul>
             </div>
-            <div class="col-md-3">
-                <ul id="menu-menu-pie" class="nav flex-column">
-                    <li id="menu-item-581" class="nav-item"><a class="nav-link" href="<?= APP_URL?>">Inicio</a></li>
-                    <li id="menu-item-582" class="nav-item"><a class="nav-link" href="<?= APP_URL?>quienes-somos/">Quienes somos</a></li>
-                    <li id="menu-item-583" class="nav-item"><a class="nav-link" href="<?= APP_URL?>politica-de-privacidad/">Política de privacidad</a></li>
-                    <li id="menu-item-584" class="nav-item"><a class="nav-link" href="<?= APP_URL?>contacto/">Contacto</a></li>
+            <div class="col-md-3 navbar-dark">
+                <ul class="flex-column navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="<?= APP_URL?>">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= APP_URL?>quienes-somos/">Quienes somos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= APP_URL?>politica-de-privacidad/">Política de privacidad</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= APP_URL?>contacto/">Contacto</a></li>
                 </ul>
             </div>
-            <div class="col-md-3">
-                <h5>Redes Sociales</h5></p>
-                <ul class="list-unstyled">
-                    <li class="mt-2"><a href="<?= $configuration['facebook_url']?>"><img class="social-logo" src="/img/facebook.png" alt="facebook">Facebook</a></li>
-                    <li class="mt-2"><a href="<?= $configuration['twitter_url']?>"><img  class="social-logo" src="/img/twitter.png" alt="twitter">Twitter</a></li>
-                    <li class="mt-2"><img class="social-logo" src="/img/whatsapp.png" alt="twitter"><?= $configuration['celular_1']?></li>
+            <div class="col-md-3 navbar-dark">
+                <h5>Redes Sociales</h5>
+                <ul class="flex-column navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="<?= $configuration['facebook_url']?>"><img class="social-logo" src="/img/facebook.png" alt="facebook">Facebook</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= $configuration['twitter_url']?>"><img  class="social-logo" src="/img/twitter.png" alt="twitter">Twitter</a></li>
+                    <li class="nav-item py-2"><img class="social-logo" src="/img/whatsapp.png" alt="twitter"><?= $configuration['celular_1']?></li>
                 </ul>
             </div>
             <div class="col-md-3">
